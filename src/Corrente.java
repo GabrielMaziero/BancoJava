942f6b83201e7b2fa0cdc2775aa130d083df5c49
Treinamento <Treinamento@RD723904.raiadrogasil.com>
2020-02-12 17:05:12 -0300

    Condição criada no deposito do cheque!

--- src/Corrente.java
+++ src/Corrente.java
@@ -6,12 +6,16 @@ public class Corrente extends Contas {
     }
 
     public void depositaCheque(float valor, String banco, String data) {
-        depositar(valor);
-        System.out.println("Cheque depositado:\n" +
-                "Banco:" + banco +
-                "\nData:" + data +
-                "\nValor:" + valor +
-                "\nSaldo: " + this.getSaldo());
+        if(valor > 0) {
+            depositar(valor);
+            System.out.println("Cheque depositado:\n" +
+                    "Banco:" + banco +
+                    "\nData:" + data +
+                    "\nValor:" + valor +
+                    "\nSaldo: " + this.getSaldo());
+        }else{
+            System.out.println("Valor do cheque invalido!");
+        }
 
     }
 
